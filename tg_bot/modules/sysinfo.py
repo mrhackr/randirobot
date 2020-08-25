b24e121dbf7f547f35573a522f0d1e3d3519f44e
mrhackr <69349061+mrhackr@users.noreply.github.com>
2020-08-25 17:45:21 +0530

    Update sysinfo.py

--- tg_bot/modules/sysinfo.py
+++ tg_bot/modules/sysinfo.py
@@ -43,7 +43,6 @@ def sysinfo(bot: Bot, update: Update):
     up = "{}\n\nUptime: {}\n".format(upt_header, pcuptime)
     c = "="*15, " CPU ", "="*15
     cpu_header = "".join(c)
-    cpumodel = cpuinfo.get_cpu_info()['brand']
     cpufreq = psutil.cpu_freq()
     cpu_info = "{}\n\n{}\nPhysical cores: {}\nTotal cores: {}\nMax Frequency: {:.2f} Mhz\nCurrent Frequency: {:.2f} Mhz\nCPU Usage: {}%\n".format(
                 cpu_header, cpumodel, psutil.cpu_count(logical=False), psutil.cpu_count(logical=True), cpufreq.max, cpufreq.current, psutil.cpu_percent(percpu=False, interval=1))
